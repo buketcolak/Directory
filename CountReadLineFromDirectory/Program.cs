20119d4ecebaf58662e433fb5eb0b12d735c5528
buketcolak <zeynpckmkk@gmail.com>
2014-07-14 13:44:40 +0300

    kaydoldu

---
+++ CountReadLineFromDirectory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace CountReadLineFromDirectory
+{
+    class Program
+    {
+       private static void Main(string[] args)
+       {
+
+           var folderPath = ConfigurationManager.AppSettings["folderPath"]; 
+
+            var files = Directory.GetFiles(folderPath, "*.txt", SearchOption.AllDirectories);
+
+            var lineCount = 0;
+            foreach (var file in files)
+            {
+                var lines = File.ReadAllLines(file);
+                lineCount += lines.Length;
+            }
+
+            Console.WriteLine("Toplam Satır Sayısı"+" "+lineCount);
+            Console.ReadLine();
+        }
+    }
+}
